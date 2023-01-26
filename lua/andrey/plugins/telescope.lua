ea29a5b710f00d16009e98feacc8f8372c07b090
Andrey Arthur <andrey_arthur1@outlook.com>
2023-01-26 16:05:15 -0300

    change telescope keybindings

--- lua/andrey/plugins/telescope.lua
+++ lua/andrey/plugins/telescope.lua
@@ -11,7 +11,7 @@ require('telescope').setup({
   }
 })
 
-vim.keymap.set('n', '<leader>tf', builtin.find_files, {})
-vim.keymap.set('n', '<leader>tg', builtin.live_grep, {})
-vim.keymap.set('n', '<leader>tb', builtin.buffers, {})
-vim.keymap.set('n', '<leader>th', builtin.help_tags, {})
+vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
+vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
+vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
+vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
