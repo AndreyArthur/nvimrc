babd7f14cc96eb868c5a68de6f40c86ec03041ee
Andrey Arthur <andrey_arthur1@outlook.com>
2023-01-26 15:18:46 -0300

    create comments with default lsp keybindings

--- lua/andrey/plugins/lsp-zero.lua
+++ lua/andrey/plugins/lsp-zero.lua
@@ -1,6 +1,18 @@
 local lsp = require('lsp-zero')
 local cmp = require('cmp')
 
+-- K: Displays hover information about the symbol under the cursor in a floating window. See :help vim.lsp.buf.hover().
+-- gd: Jumps to the definition of the symbol under the cursor. See :help vim.lsp.buf.definition().
+-- gD: Jumps to the declaration of the symbol under the cursor. Some servers don't implement this feature. See :help vim.lsp.buf.declaration().
+-- gi: Lists all the implementations for the symbol under the cursor in the quickfix window. See :help vim.lsp.buf.implementation().
+-- go: Jumps to the definition of the type of the symbol under the cursor. See :help vim.lsp.buf.type_definition().
+-- gr: Lists all the references to the symbol under the cursor in the quickfix window. See :help vim.lsp.buf.references().
+-- <Ctrl-k>: Displays signature information about the symbol under the cursor in a floating window. See :help vim.lsp.buf.signature_help(). If a mapping already exists for this key this function is not bound.
+-- <F2>: Renames all references to the symbol under the cursor. See :help vim.lsp.buf.rename().
+-- <F4>: Selects a code action available at the current cursor position. See :help vim.lsp.buf.code_action().
+-- gl: Show diagnostics in a floating window. See :help vim.diagnostic.open_float().
+-- [d: Move to the previous diagnostic in the current buffer. See :help vim.diagnostic.goto_prev().
+-- ]d: Move to the next diagnostic. See :help vim.diagnostic.goto_next().
 
 lsp.preset('recommended')
 -- Fix all eslint issues on save
@@ -37,6 +49,11 @@ lsp.setup_nvim_cmp({
     -- Accept currently selected item. If none selected, `select` first item.
     -- Set `select` to `false` to only confirm explicitly selected items.
     ['<tab>'] = cmp.mapping.confirm { select = true },
+  },
+  sources = {
+    { name = "nvim_lsp" },
+    { name = "buffer" },
+    { name = "path" },
   },
 });
 lsp.setup()
