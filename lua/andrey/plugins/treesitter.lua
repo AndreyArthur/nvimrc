5414ba8d354b5b7c60692df5725574a9be1a208b
Andrey Arthur <andrey_arthur1@outlook.com>
2023-09-03 22:41:53 -0300

    use treesitter text objects

--- lua/andrey/plugins/treesitter.lua
+++ lua/andrey/plugins/treesitter.lua
@@ -1,9 +1,46 @@
-require'nvim-treesitter.configs'.setup {
-  ensure_installed = { 'help', 'javascript', 'typescript', 'c', 'lua', 'vim' },
-  sync_install = false,
-  auto_install = true,
-  highlight = {
-    enable = true,
-    additional_vim_regex_highlighting = false,
+require('nvim-treesitter.configs').setup({
+  ensure_installed = { 'c', 'cpp', 'go', 'lua', 'python', 'rust', 'tsx', 'typescript', 'vimdoc', 'vim' },
+  auto_install = false,
+  highlight = { enable = true },
+  indent = { enable = true },
+  incremental_selection = {
+    enable = false,
   },
-}
+  textobjects = {
+    select = {
+      enable = true,
+      lookahead = true,
+      keymaps = {
+        ['aa'] = '@parameter.outer',
+        ['ia'] = '@parameter.inner',
+        ['af'] = '@function.outer',
+        ['if'] = '@function.inner',
+        ['ac'] = '@class.outer',
+        ['ic'] = '@class.inner',
+      },
+    },
+    move = {
+      enable = true,
+      set_jumps = true,
+      goto_next_start = {
+        [']m'] = '@function.outer',
+        [']]'] = '@class.outer',
+      },
+      goto_next_end = {
+        [']M'] = '@function.outer',
+        [']['] = '@class.outer',
+      },
+      goto_previous_start = {
+        ['[m'] = '@function.outer',
+        ['[['] = '@class.outer',
+      },
+      goto_previous_end = {
+        ['[M'] = '@function.outer',
+        ['[]'] = '@class.outer',
+      },
+    },
+    swap = {
+      enable = false,
+    },
+  },
+})
