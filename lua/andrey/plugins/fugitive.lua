bbd511337385ec7391a6a143b619a1ef54cea162
Andrey Arthur <andrey_arthur1@outlook.com>
2023-10-06 13:26:00 -0300

    create a toggle command for fugitive

--- lua/andrey/plugins/fugitive.lua
+++ lua/andrey/plugins/fugitive.lua
@@ -1,1 +1,9 @@
-vim.api.nvim_set_keymap('n', '<leader>g', ':Git<cr>', { noremap = true })
+local toggle_fugitive = function ()
+  if (vim.bo.filetype == 'fugitive') then
+    vim.cmd('quit')
+    return
+  end
+  vim.cmd('Git')
+end
+
+vim.keymap.set('n', '<leader>g', toggle_fugitive, { noremap = true })
