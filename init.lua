4c12be644e1e881eb83b90685ad2a7f608358b84
Andrey Arthur <andrey_arthur1@outlook.com>
2023-10-22 21:09:50 -0300

    create a function and a keymap to resource neovim config whithout quiting

--- init.lua
+++ init.lua
@@ -5,3 +5,23 @@ require('andrey.plugins.load')
 require('andrey.colorscheme')
 require('andrey.terminal')
 require('andrey.gui')
+
+
+local source = function ()
+  local command = "find ~/.config/nvim/lua/**/* ~/.config/nvim/init.lua -type f"
+  local output = io.popen(command)
+  if (output == nil) then
+    return
+  end
+  local files_string = output:read("*a")
+  output:close()
+  local files = {}
+  for line in files_string:gmatch("[^\n]+") do
+    table.insert(files, line)
+  end
+  for _, file in ipairs(files) do
+    vim.cmd('source ' .. file)
+  end
+end
+
+vim.keymap.set('n', '<leader><F5>', source, { silent = true, noremap = true });
