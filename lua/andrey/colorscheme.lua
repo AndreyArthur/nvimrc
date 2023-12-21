940c89130bac033ad3d952256b27f0a75bf67ca5
Andrey Arthur <andrey_arthur1@outlook.com>
2023-12-21 10:57:05 -0300

    simplify colorscheme configuration

--- lua/andrey/colorscheme.lua
+++ lua/andrey/colorscheme.lua
@@ -1,17 +1,5 @@
-require('onedark').setup({
-  style = 'warm', -- dark, darker, cool, deep, warm or warmer
-})
 require('tokyonight').setup({
-  style = 'moon', -- storm, night, moon, or day
+  style = 'moon',
 })
-require('gruvbox').setup({
-  constrast = 'hard', -- hard, soft or <empty>
-})
-require('kanagawa').load('lotus') -- wave, dragon or lotus
 
-local colorscheme = 'tokyonight'
-
-local status_ok, _ = pcall(vim.cmd, 'colorscheme ' .. colorscheme)
-if not status_ok then
-  return
-end
+vim.cmd('colorscheme tokyonight')
