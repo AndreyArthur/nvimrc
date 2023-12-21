77dff8ebe197b53307c5361e88b04ac34fb647e2
Andrey Arthur <andrey_arthur1@outlook.com>
2023-12-21 10:55:52 -0300

    change neovide font

--- lua/andrey/gui.lua
+++ lua/andrey/gui.lua
@@ -1,7 +1,6 @@
 if vim.g.neovide then
-  vim.o.guifont = 'Fantasque Sans Mono Regular:h10'
+  vim.o.guifont = 'FantasqueSansM Nerd Font Mono:h12.2'
   vim.g.neovide_scroll_animation_length = 0.3
   vim.g.neovide_cursor_animation_length = 0.05
   vim.g.neovide_cursor_trail_size = 0.4
 end
-
