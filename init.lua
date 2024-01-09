be77b4e6dc171f135729fdf6836a072889d238d6
Andrey Arthur <andrey_arthur1@outlook.com>
2024-01-09 13:16:00 -0300

    fix with stylua

--- init.lua
+++ init.lua
@@ -6,17 +6,16 @@ require('andrey.colorscheme')
 require('andrey.terminal')
 require('andrey.gui')
 
-
-local source = function ()
-  local command = "find ~/.config/nvim/lua/**/* ~/.config/nvim/init.lua -type f"
+local source = function()
+  local command = 'find ~/.config/nvim/lua/**/* ~/.config/nvim/init.lua -type f'
   local output = io.popen(command)
-  if (output == nil) then
+  if output == nil then
     return
   end
-  local files_string = output:read("*a")
+  local files_string = output:read('*a')
   output:close()
   local files = {}
-  for line in files_string:gmatch("[^\n]+") do
+  for line in files_string:gmatch('[^\n]+') do
     table.insert(files, line)
   end
   for _, file in ipairs(files) do
@@ -24,4 +23,4 @@ local source = function ()
   end
 end
 
-vim.keymap.set('n', '<leader><F5>', source, { silent = true, noremap = true });
+vim.keymap.set('n', '<leader><F5>', source, { silent = true, noremap = true })
