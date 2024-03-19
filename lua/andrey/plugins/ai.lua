7df456f2aca4a4668b829700ccd2fed0504308d3
Andrey Arthur <andrey_arthur1@outlook.com>
2024-03-19 11:50:36 -0300

    add a keymap to disable codeium

--- lua/andrey/plugins/ai.lua
+++ lua/andrey/plugins/ai.lua
@@ -1,5 +1,11 @@
 vim.g.codeium_disable_bindings = 1
 
+vim.keymap.set('i', '<c-f>t', function()
+  return vim.cmd('CodeiumToggle')
+end, { expr = true, silent = true })
+vim.keymap.set('i', '<c-f><c-t>', function()
+  return vim.cmd('CodeiumToggle')
+end, { expr = true, silent = true })
 vim.keymap.set('i', '<c-f>h', function()
   return vim.fn['codeium#Clear']()
 end, { expr = true, silent = true })
