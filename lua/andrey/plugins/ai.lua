9945a25792eb7b509c03e7514415cc03a7db9845
Andrey Arthur <andrey_arthur1@outlook.com>
2024-03-23 19:22:10 -0300

    disable ai by default

--- lua/andrey/plugins/ai.lua
+++ lua/andrey/plugins/ai.lua
@@ -1,11 +1,14 @@
 vim.g.codeium_disable_bindings = 1
 
+vim.cmd('CodeiumDisable')
+
 vim.keymap.set('i', '<c-f>t', function()
   return vim.cmd('CodeiumToggle')
 end, { expr = true, silent = true })
 vim.keymap.set('i', '<c-f><c-t>', function()
   return vim.cmd('CodeiumToggle')
 end, { expr = true, silent = true })
+
 vim.keymap.set('i', '<c-f>h', function()
   return vim.fn['codeium#Clear']()
 end, { expr = true, silent = true })
