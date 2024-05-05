e9ee3a3c4db99101c3d2ee31d3d93ec42c6a6614
Andrey Arthur <andrey_arthur1@outlook.com>
2024-05-05 13:30:24 -0300

    make eslint just be active when theres an eslintrc

--- lua/andrey/plugins/null-ls.lua
+++ lua/andrey/plugins/null-ls.lua
@@ -1,11 +1,37 @@
 local null_ls = require('null-ls')
 
+local file_exists = function(file)
+  local f = io.open(file, 'r')
+  if f ~= nil then
+    io.close(f)
+    return true
+  else
+    return false
+  end
+end
+
+local eslint_config = {
+  extra_args = function(params)
+    local file_types = { 'js', 'cjs', 'yaml', 'yml', 'json' }
+    for _, file_type in pairs(file_types) do
+      if file_exists(params.root .. '/.eslintrc.' .. file_type) then
+        return {}
+      end
+    end
+
+    return {
+      '--no-config-lookup',
+    }
+  end,
+}
+
 null_ls.setup({
   sources = {
     null_ls.builtins.formatting.stylua,
-    null_ls.builtins.diagnostics.eslint_d,
-    null_ls.builtins.formatting.eslint_d,
+    null_ls.builtins.diagnostics.eslint_d.with(eslint_config),
+    null_ls.builtins.formatting.eslint_d.with(eslint_config),
     null_ls.builtins.formatting.gofmt,
+    null_ls.builtins.formatting.clang_format,
   },
 })
 
