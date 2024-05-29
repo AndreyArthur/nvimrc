94374eec63dda39ab6b4aa69186280d2aff07e3b
Andrey Arthur <andrey_arthur1@outlook.com>
2024-05-29 08:52:08 -0300

    remove gui config

--- lua/src/gui.lua
+++
@@ -1,6 +0,0 @@
-if vim.g.neovide then
-  vim.o.guifont = 'FantasqueSansM Nerd Font Mono:h12.2'
-  vim.g.neovide_scroll_animation_length = 0.3
-  vim.g.neovide_cursor_animation_length = 0.05
-  vim.g.neovide_cursor_trail_size = 0.4
-end
