f5beb6643227196bdfa8649b12ef5d7563f2445c
Andrey Arthur <andrey_arthur1@outlook.com>
2024-06-10 14:41:40 -0300

    move indent blankline and splitjoin to an indentation file

--- lua/plugins/indentation.lua
+++ lua/plugins/indentation.lua
@@ -1,9 +1,19 @@
 local splitjoin = require('mini.splitjoin')
+local indent_blankline = require('ibl')
 
 splitjoin.setup({
   mappings = {
     toggle = '<leader>ss',
     split = '',
     join = '',
+  },
+})
+
+indent_blankline.setup({
+  indent = {
+    char = '▏',
+  },
+  scope = {
+    enabled = false,
   },
 })
