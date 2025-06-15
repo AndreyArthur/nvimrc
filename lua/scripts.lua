d0bfb2ff68c49f26934b5e4a709be41c058df8ab
Andrey Arthur <andrey_arthur1@outlook.com>
2025-06-15 11:57:33 -0300

    fix border colors when entering neovim

--- lua/scripts.lua
+++ lua/scripts.lua
@@ -3,6 +3,20 @@ vim.api.nvim_create_autocmd('VimEnter', {
   callback = function() vim.api.nvim_set_current_dir(vim.fn.expand('%:p:h')) end,
 })
 
+vim.api.nvim_create_autocmd({ 'UIEnter', 'ColorScheme' }, {
+  callback = function()
+    local normal = vim.api.nvim_get_hl(0, { name = 'Normal' })
+    if not normal.bg then
+      return
+    end
+    io.write(string.format('\027]11;#%06x\027\\', normal.bg))
+  end,
+})
+
+vim.api.nvim_create_autocmd('UILeave', {
+  callback = function() io.write('\027]111\027\\') end,
+})
+
 local source = function()
   local command = 'find ~/.config/nvim/lua/**/* ~/.config/nvim/init.lua -type f'
   local output = io.popen(command)
